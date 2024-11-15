f7689ccf9db617cf4cf9141e00f3bcbe21b24a3c
Liam Rall <liam.rall@ymail.com>
2024-11-15 06:39:52 +0200

    add basic calculator function
    - User can now do basic calculator functions, and it allows for multiple
      entries.
    - Need to work on the functional buttons next

--- js-file.js
+++ js-file.js
@@ -19,6 +19,8 @@ function divide(num1, num2) {
 
 function operate(num1, num2, operator) {
     let result = 0;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
     switch (operator) {
         case '+':
             result = add(num1, num2);
@@ -26,7 +28,7 @@ function operate(num1, num2, operator) {
         case '-':
             result = subtract(num1, num2);
             break;
-        case '*':
+        case 'x':
             result = multiply(num1, num2);
             break;
         case '/':
@@ -37,10 +39,68 @@ function operate(num1, num2, operator) {
     }
     return result;
 }
-const result = document.getElementById("result");
-const numberButton = document.querySelectorAll('.button.number');
+
+function updateScreen(value){
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = value;
+}
+
+let num1 = ""
+let num2 = ""
+let operator = "";
+let screenValue = "";
+
+
+const numberButton = document.querySelectorAll('.number');
 numberButton.forEach((button) => {
     button.addEventListener('click', () => {
-        result.textContent = button.textContent;
+        const inputValue = button.textContent;
+        if (num1 === "") {
+            num1 = inputValue;
+            screenValue = inputValue;
+            updateScreen(screenValue);
+        } else if(num2 === "" && operator === "") {
+            num1 += inputValue;
+            screenValue += inputValue;
+            updateScreen(screenValue);
+        } else if(num2 === ""){
+            num2 = inputValue;
+            screenValue = inputValue;
+            updateScreen(screenValue);
+        }
+        else {
+            num2 += inputValue;
+            screenValue += inputValue;
+            updateScreen(screenValue);
+        }
     });
 });
+
+
+const operatorButton = document.querySelectorAll('.operator');
+operatorButton.forEach((button) => {
+    button.addEventListener('click', () => {
+        const currentOperator = button.textContent;
+        switch (currentOperator) {
+            case '=':
+                screenValue = operate(num1, num2, operator);
+                updateScreen(screenValue);
+                num1 = screenValue;
+                num2 = "";
+                operator = "";
+                break;
+            default:
+                if (operator === ""){
+                    operator = currentOperator;
+                } else if(operator !== ""){
+                    screenValue = operate(num1, num2, operator);
+                    updateScreen(screenValue);
+                    num1 = screenValue;
+                    num2 = "";
+                    operator = currentOperator;
+                }
+                break;
+        }
+    });
+});
+
